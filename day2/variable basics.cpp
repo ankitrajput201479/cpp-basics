30612a42dc1d10fea4ee4e91bf811f9edf1b9a5a
ankitrajput201479 <ankitrajput201479@gmail.com>
2025-12-23 16:33:47 +0530

    variable practice
    This program showcases various fundamental data types in C++, including integers, characters, floats, and booleans.

---
+++ day2/variable basics.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+  
+   //this program demonstrates different data types in C++
+   int a = 14;
+   int b = 34;
+   char g = 'q';
+   float pi = 3.14;
+   bool z = true;
+   
+   cout<<"the value of a is "<<a;
+   cout<<" \n the value of b is "<<b;
+   cout<<" \n the value of character g is "<<g;
+   cout<<" \n the value of pi is"<<pi;
+    cout<<" \n the value of boolean z is "<<z;
+
+    return 0;
+}
