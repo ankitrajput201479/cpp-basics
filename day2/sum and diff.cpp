32cf61db2bf00090c7a74caab0d542e0b2034700
ankitrajput201479 <ankitrajput201479@gmail.com>
2025-12-23 16:46:13 +0530

    day 2 sum and difference of two numbers in c++
    This program performs basic mathematical operations: addition and subtraction.

---
+++ day2/sum and diff.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  
+   //this program is about mathmaical operations in c++
+   int a = 14;
+   int b = 34;
+   int sum = a + b;
+   int diff = b - a;
+   cout<<"the difference of b and a is "<<diff;
+   cout<<" \n the sum of a and b is "<<sum;
+   cout<<"\n the value of a is "<<a;
+   cout<<" \n the value of b is "<<b;
+
+return 0;
+}
