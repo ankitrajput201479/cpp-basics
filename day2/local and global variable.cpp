5b0b06540097fabf3af0d0f9c680e1ad7174b438
ankitrajput201479 <ankitrajput201479@gmail.com>
2025-12-23 17:07:00 +0530

    local and global variable

--- day2/local and global variable.cpp
+++ day2/local and global variable.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
-    int glo=45;
+    int glo=45; // assigning global variable outside function
     void sum(){
         cout<<"\n this is global variable "<<glo;        
     }
 int main() {
-    int glo=66;
+    int glo=66; // assigning local variable inside function
     glo=44;
     cout<<" \n this is local variable "<<glo;
-    sum();
+    sum(); // calling function
     return 0;
 }
+// local and global variable can have same name
