7ab8e84bb86611bf5036d401f8d3ea0c4c41f374
ankitrajput201479 <ankitrajput201479@gmail.com>
2025-12-23 17:04:12 +0530

    different types of data types
    Added comments for clarity and improved code readability.

--- day2/data types.cpp
+++ day2/data types.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
+#include <iostream>  //this program demonstrates different data types in C++
 using namespace std;
 int main() {
   
-   //this program demonstrates different data types in C++
-   int a = 14;
+   // assigning variables and their values
+   int a = 14; 
    int b = 34;
    char g = 'q';
    float pi = 3.14;
    bool z = true;
-   
+   // getting values of variables
    cout<<"the value of a is "<<a;
    cout<<" \n the value of b is "<<b;
    cout<<" \n the value of character g is "<<g;
