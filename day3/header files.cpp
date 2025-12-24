e482e13fdf041ce05602c5ade4e71119877818c3
ankitrajput201479 <ankitrajput201479@gmail.com>
2025-12-24 14:58:35 +0530

    Create header files.cpp with user-defined header

---
+++ day3/header files.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"my name.h"
+using namespace std;
+int main(){
+    cout<<"this is fro us";
+
+    return 0;}
+/* }   before making a header file error was showing like this' fatal error: my name: No such file or directory
+ #include"my name"'but aftr making a file named "my name" in the same directory as this file the error is resolved.
+this is called used defined header file.*/
