1458a69752a5fa71b0d0a8bc3f8fc512d5e8a098
Ankit Chauhan <ankitrajput201479@gmail.com>
2025-12-25 13:37:29 +0530

    precedence

---
+++ day4/precedence.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    float a=5,b=45;
+    float c=(a+(b*66)+43)/b;
+    cout<<"the value of c is :"<<setw(10)<<c<<endl;
+return 0;
+}
