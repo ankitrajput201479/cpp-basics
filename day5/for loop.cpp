0810f8426b78e95b880e0eab39ce93455c7ac018
Ankit Chauhan <ankitrajput201479@gmail.com>
2025-12-26 15:38:56 +0530

    for loop to display multiples of 6
    Implement for loop to print multiples of 6 in both ascending and descending order.

---
+++ day5/for loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+for (int i =6; i<=60; i+=6){  //6 multiples using for loop
+    cout<<i<<endl;
+    }// in  reverse order
+cout<<"reverse order:\n";
+for(int i=60; i>=6; i-=6){
+    
+    cout<<i<<endl;
+}
+    return 0;
+}
