c22218247bffadc20feeef2b61bc5a93564f63c3
Ankit Chauhan <ankitrajput201479@gmail.com>
2025-12-26 15:40:09 +0530

    while loop for multiples of 6
    Implement while loop to print multiples of 6 in both ascending and descending order.

---
+++ day5/while loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){  //6 multiples using while loop
+    int i=6;
+    while(i<=60){
+        cout<<i<<endl;
+        i=i+6;
+    }
+    cout<<"reverse order:\n";
+    i=60;
+    while(i>=6){
+        cout<<i<<endl;
+        i =i-6;
+    }
+return 0;
+}
