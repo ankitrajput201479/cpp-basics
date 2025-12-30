43944d7d15c1e47ec5436051ba2f1d58a2ee49b6
Ankit Chauhan <ankitrajput201479@gmail.com>
2025-12-30 15:25:29 +0530

    array sum calculation in C++
    sum calculation of all array elements.

---
+++ day9/array sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int arr[5] ={20,30,40,50,60};
+   int sum = 0;
+   for (int i=0;i<=4;i++){
+      sum = sum+arr[i];
+      
+   }
+cout<<"sum of arrays is:  "<<sum;
+   return 0;
+}
