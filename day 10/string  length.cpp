e74b39ccc3a0d43d737ccbee27e161d5cb344139
Ankit Chauhan <ankitrajput201479@gmail.com>
2025-12-31 13:14:28 +0530

    string length calculation in C++
    Implement a program to calculate string length.

---
+++ day 10/string  length.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[] = {"ankit"};
+   int  count =0;
+    for(int i = 0;str[i]!='\0';i++){
+        count++;
+    }
+   cout<<count;
+
+   return 0;
+}
